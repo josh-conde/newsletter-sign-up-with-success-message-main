7d61b94aa276d61339883035f19b4e0f050a37df
joshconde <joshconde@aol.com>
2024-05-15 11:03:08 -0400

    Final 1

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const form = document.getElementById('form');
+const success = document.getElementById('success-message');
+const hideMain = document.getElementById('main-container');
+
 function handleSubmit(e) {
     e.preventDefault();
+    success.classList.toggle('hidden');
+    hideMain.classList.toggle('hidden');
 }
+
 form.addEventListener('submit', handleSubmit);
